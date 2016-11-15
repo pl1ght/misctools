9d1abcddcea409f244694183339fe6e44a4e8e2a
pl1ght <forsaken@4saken.org>
2016-11-15 12:52:58 -0600

    trello messin

---
+++ trello.rb
@@ -0,0 +1,26 @@
+require 'trello'
+
+devKey = "3018d092aac2c2df60b4d58b8a862aab"
+memberKey = "5caf135de6ff99c5fe35ede4227d1d95393b42adccbfa01608f68340ab3d2822"
+
+# Configure Trello client connection
+Trello.configure do |config|
+  config.developer_public_key = devKey
+  config.member_token = memberKey
+end
+
+me = Trello::Member.find("davewithers3")
+
+board = me.boards[3]
+puts board.name
+
+board.cards.each do |card|
+  puts "- \"#{card.name}\""
+end
+
+board.lists.each do |list|
+  puts "- \"#{list.name}\""
+end
+
+
+
