4f978491366aaa5f263b6d1774667d0a9d5e3624
pl1ght <forsaken@4saken.org>
2017-01-23 15:19:11 -0600

    trello messin

--- trello.rb
+++ trello.rb
@@ -1,15 +1,15 @@
 require 'trello'
 
-devKey = "3018d092aac2c2df60b4d58b8a862aab"
-memberKey = "5caf135de6ff99c5fe35ede4227d1d95393b42adccbfa01608f68340ab3d2822"
+dev_key = '3018d092aac2c2df60b4d58b8a862aab'
+member_key = '5caf135de6ff99c5fe35ede4227d1d95393b42adccbfa01608f68340ab3d2822'
 
 # Configure Trello client connection
 Trello.configure do |config|
-  config.developer_public_key = devKey
-  config.member_token = memberKey
+  config.developer_public_key = dev_key
+  config.member_token = member_key
 end
 
-me = Trello::Member.find("davewithers3")
+me = Trello::Member.find('davewithers3')
 
 board = me.boards[3]
 puts board.name
@@ -21,6 +21,3 @@ end
 board.lists.each do |list|
   puts "- \"#{list.name}\""
 end
-
-
-
